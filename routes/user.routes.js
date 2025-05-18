971b81face78d70763710962e9b4877bb9a229cd
musamusakannike <musamusakannike@gmail.com>
2025-05-18 08:54:48 +0100

    Rearranged routes to avoid route mismatch

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -16,15 +16,15 @@ const router = express.Router()
 // All routes are protected
 router.use(protect)
 
-// Admin and superadmin routes
-router.get("/", isAdminOrSuperadmin, getUsers)
-router.get("/:id", isAdminOrSuperadmin, getUserById)
-router.put("/:id", isAdminOrSuperadmin, updateUser)
-
 // Superadmin only routes
 router.get("/admins", isSuperadmin, getAdmins)
 router.post("/create-admin", isSuperadmin, registerValidation, validate, createAdmin)
 router.put("/:id/role", isSuperadmin, updateUserRole)
 router.delete("/:id", isSuperadmin, deleteUser)
+
+// Admin and superadmin routes
+router.get("/", isAdminOrSuperadmin, getUsers)
+router.get("/:id", isAdminOrSuperadmin, getUserById)
+router.put("/:id", isAdminOrSuperadmin, updateUser)
 
 export default router
